71c124efbc825fb395e60395faea352b9c3e7252
nosz <nosz.mail@web.de>
2025-02-17 00:15:54 +0100

    offline;service-worker

--- service-worker.js
+++ service-worker.js
@@ -1,69 +1,54 @@
-console.log("hello from service-worker.js");
-var CACHE_NAME = "static-cache";
-var urlsToCache = [
-    ".",
-    "index.html",
-    "languages.js",
-    "energystation.js",
-    "js/hammer.min.js",
-    "js/index.js",
-    "js/jquery.min.js",
-    "js/particles.min.js",
-    "css/style.css",
-    "css/bootstrap.min.css",
-    "schriften/dancing_script.css",
-    "schriften/tangerine_script.css",
-    "img/xhumor.jpg"
-
+const CACHE_NAME = "offline-cache-v1";
+const OFFLINE_URL = "offline.html";
+const FILES_TO_CACHE = [
+  "/",
+  "/index.html",
+  "/css/style.css",
+  "/js/index.js",
+  "/images/icon.png",
+  OFFLINE_URL
 ];
 
-self.addEventListener("load", function(event) {
-    console.log("service-worker.js load");
-})
-
-self.addEventListener("install", function(event) {
-    console.log("neu service-worker.js listener install")
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-        .then(function(cache) {
-            console.log("in then...service-worker.js")
-            return cache.addAll(urlsToCache);
-        })
-    );
-});
-
-self.addEventListener("fetch", function(event) {
-    console.log("fetch");
-    event.respondWith(
-        caches.match(event.request)
-        .then(function(response){
-            return response || fetchAndCache(event.request);
-        })
-    );
-});
-
-function fetchAndCache(url) {
-    console.log("fetchAndCache");
-    return fetch(url)
-    .then(function(response) {
-      // Check if we received a valid response
-      if (!response.ok) {
-        throw Error(response.statusText);
-      }
-      return caches.open(CACHE_NAME)
-      .then(function(cache) {
-        cache.put(url, response.clone());
-        return response;
-      });
+// Installations-Event
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(FILES_TO_CACHE);
     })
-    .catch(function(error) {
-      console.log('Request failed:', error);
-      // You could return a custom offline 404 page here
-    });
+  );
+});
+
+// Aktivierungs-Event
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Fetch-Event (Netzwerk-Anfragen abfangen)
+self.addEventListener("fetch", event => {
+  if (!event.request.url.startsWith("http")) {
+    return;
+  }
   
-}
-
-
-
-
-
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        return caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request, response.clone());
+          return response;
+        });
+      })
+      .catch(() => caches.match(event.request).then(response => {
+        return response || caches.match(OFFLINE_URL);
+      }))
+  );
+});
