9cc0208616f2de11c30da3d8c06809e8d23a3b0a
nosz <nosz.mail@web.de>
2025-02-17 00:48:17 +0100

    js/index.js

--- js/index.js
+++ js/index.js
@@ -116,7 +116,7 @@ particlesJS("particles-js", {
 /* ---- stats.js config ---- */
 
 var count_particles, stats, update;
-stats = new Stats;
+stats = new stats;
 stats.setMode(0);
 stats.domElement.style.position = 'absolute';
 stats.domElement.style.left = '0px';
